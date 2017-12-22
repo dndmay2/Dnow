043528bbe245a2529007a85d8fc14ae352dcc0f7
Derek May <dndmay2@gmail.com>
2017-12-21 22:09:37 -0600

    Turned debug on

--- churchsite/settings/production.py
+++ churchsite/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 import dj_database_url
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # import string,random; uni=string.ascii_letters+string.digits+string.punctuation; print repr(''.join([random.SystemRandom().choice(uni) for i in range(random.randint(45,50))]))
